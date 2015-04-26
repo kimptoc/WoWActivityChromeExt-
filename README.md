55e9c6dd0d61487f7c79bd42bfec24d9cac172b8
Chris Kimpton <chris@kimptoc.net>
2015-04-26 08:47:20 +0100

    WIP

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
+
+WoW Armory - (eg US) http://us.battle.net/wow/en/community/  , sample toon - http://us.battle.net/wow/en/character/turalyon/deatt/simple
+
+WoW Activity Feed - http://wowactivity.kimptoc.net/
+
 TODO
 
-* check if url is an armory page
-  * if not, hide/disable button
-  * if it is enable button
 * when button clicked
   * open new tab showing corresponding WAF page (is that allowed)
+* tweak button based on url
+  * if not, hide/disable button
+  * if it is enable button
 * do we need to have popup.html - can it be done from the extension button alone
+
+DONE
+
+* how to debug - hopefully console.log works (it does). Also can right click on button to get inspect popup debug window
+* check if url is an armory page
---
+++ popup.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+
+  chrome.tabs.getSelected(null, function(tab) {
+
+    // are we on a battle.net page?
+    if (/\.battle\.net\/wow/.test(tab.url))
+    {
+      console.log("XX battle net char:"+tab.url);
+      // TODO - setup button
+    } else {
+      console.log("XX NOT a battle net char:"+tab.url);
+      // TODO - hide button
+    }
+
+    // d = document;
+    //
+    // var f = d.createElement('form');
+    // f.action = 'http://gtmetrix.com/analyze.html?bm';
+    // f.method = 'post';
+    // var i = d.createElement('input');
+    // i.type = 'hidden';
+    // i.name = 'url';
+    // i.value = tab.url;
+    // f.appendChild(i);
+    // d.body.appendChild(f);
+    // f.submit();
+  });
+
+
+
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+
+      // are we on a battle.net page?
+      if (/\.battle\.net\/wow/.test(tab.url))
+      {
+        console.log("battle net char:"+tab.url);
+      } else {
+        console.log("NOT a battle net char:"+tab.url);
+      }
+
+      // d = document;
+      //
+      // var f = d.createElement('form');
+      // f.action = 'http://gtmetrix.com/analyze.html?bm';
+      // f.method = 'post';
+      // var i = d.createElement('input');
+      // i.type = 'hidden';
+      // i.name = 'url';
+      // i.value = tab.url;
+      // f.appendChild(i);
+      // d.body.appendChild(f);
+      // f.submit();
+    });
+  }, false);
+}, false);
